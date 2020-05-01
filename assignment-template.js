d8cb75fdfae8cd06baf03bac03761d3b620d677d
Kimberly <kpierce865@yahoo.com>
2020-04-30 21:56:14 -0400

    commit

---
+++ assignment-template.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment
+; Author: Kimberly Pierce
+; Date: 19 February 2020
+; Modified By: Kimberly Pierce
+; Description:
+;===========================================
+*/
+
+const header = require('../pierce-header.js'); //import statement for header file
+console.log(header.display("Kimberly", "Pierce", "Assignment ") + " \n ");
